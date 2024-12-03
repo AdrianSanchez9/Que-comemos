be196ea1bb207d6c76f9cd7152046ccea880262a
AdrianSanchez9 <adrian.nicolais@gmail.com>
2024-12-03 20:45:29 -0300

    nav

--- que-comemos/src/app/app.routes.ts
+++ que-comemos/src/app/app.routes.ts
@@ -1,3 +1,11 @@
+// src/app/app.routes.ts
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { 
+    path: '', 
+    loadChildren: () => import('./features/Auth/auth.routes').
+        then(m => m.AUTH_ROUTES)
+  },
+];
+
