c9d343c7e1f1931d9353ed1e4d3c2b5da0aa1986
AdrianSanchez9 <adrian.nicolais@gmail.com>
2024-12-04 12:35:52 -0300

    Primera parte del login

--- que-comemos/src/app/features/Auth/login/login.component.ts
+++ que-comemos/src/app/features/Auth/login/login.component.ts
@@ -1,11 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  
+  private formBuilder = inject(FormBuilder);
+
+   loginForm =  this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    }); 
+
+  constructor (private router: Router) {}
+
+  get email () {
+    return this.loginForm.controls.email;
+  }
+
+  
+  get password () {
+    return this.loginForm.controls.password;
+  }
+    
+  login (){
+    if (this.loginForm.valid){
+      console.log("Se puede iniciar Sesión Correctamente");
+      this.router.navigate(['/']);
+      this.loginForm.reset();
+    }
+    else{
+      this.loginForm.markAllAsTouched();
+      console.log("No se puede iniciar Sesión");
+    }
+  }
+
+
+
 
 }
