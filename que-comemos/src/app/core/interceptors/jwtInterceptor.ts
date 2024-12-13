ed900b232314c78a9c5f4d3f93b9719a0e6c1c79
AdrianSanchez9 <adrian.nicolais@gmail.com>
2024-12-12 22:36:31 -0300

    Modifico interceptor Jwt

--- que-comemos/src/app/core/interceptors/jwtInterceptor.ts
+++ que-comemos/src/app/core/interceptors/jwtInterceptor.ts
@@ -1,9 +1,13 @@
 import { HttpEvent, HttpRequest, HttpHandlerFn, HttpInterceptorFn } from '@angular/common/http';
 import { inject } from '@angular/core';
 import { LoginService } from '../services/auth/login.service';
+import { catchError } from 'rxjs/operators'; 
+import { throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 export const jwtInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: HttpHandlerFn) => {
   const loginService = inject(LoginService); 
+  const router = inject(Router);
   const token: String = loginService.userToken;
   console.log('Adding token:', token);
 
@@ -18,5 +22,14 @@ export const jwtInterceptor: HttpInterceptorFn = (req: HttpRequest<any>, next: H
     });
   }
 
-  return next(req); 
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status === 401) { // Token expirado o no autorizado
+        console.warn('Token expired or unauthorized. Redirecting to login.');
+        loginService.logout(); // Limpia cualquier dato relacionado al usuario
+        router.navigate(['/auth/login']); // Redirige al login
+      }
+      return throwError(() => error); // Lanza el error para que el componente lo maneje
+    })
+  );
 };
